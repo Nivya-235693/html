a1e5d774b4caab58f10de49dd5591ab0a57607f6
Nivya-235693 <112929554+Nivya-235693@users.noreply.github.com>
2022-10-06 17:54:06 +0530

    change

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function onFormSubmit(e) {
 //Retrieve the data
 function readFormData() {
     var formData = {};
-    formData["productCode"] = document.getElementById("productCode").value;
-    formData["product"] = document.getElementById("product").value;
-    formData["qty"] = document.getElementById("qty").value;
-    formData["perPrice"] = document.getElementById("perPrice").value;
+    formData["rollno"] = document.getElementById("rollno").value;
+    formData["name"] = document.getElementById("name").value;
+    formData["m1"] = document.getElementById("m1").value;
+    formData["m2"] = document.getElementById("m2").value;
     return formData;
 }
 
@@ -41,10 +41,10 @@ function insertNewRecord(data) {
 //Edit the data
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("rollno").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("name").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("m1").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("m2").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.productCode;
@@ -54,19 +54,20 @@ function updateRecord(formData) {
 }
 
 //Delete the data
-function onDelete(td) {
+function onDelete(td){
     if (confirm('Do you want to delete this record?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
-        resetForm();
+        ckList = document.querySelectorAll("input[type=checkbox]:checked");
+        ckList.forEach(function(el){
+            el.parentElement.parentElement.remove();
+        });
     }
 }
 
 //Reset the data
 function resetForm() {
-    document.getElementById("productCode").value = '';
-    document.getElementById("product").value = '';
-    document.getElementById("qty").value = '';
-    document.getElementById("perPrice").value = '';
+    document.getElementById("rollno").value = '';
+    document.getElementById("name").value = '';
+    document.getElementById("m1").value = '';
+    document.getElementById("m2").value = '';
     selectedRow = null;
 }
