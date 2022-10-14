5f60c9db5b3fc7e80315a74b2099eeaf2446394c
Nivya-235693 <112929554+Nivya-235693@users.noreply.github.com>
2022-10-14 15:31:47 +0530

    chnage

--- angular/todo/src/app/home/home.component.ts
+++ angular/todo/src/app/home/home.component.ts
@@ -11,17 +11,22 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-    title = 'first_project';
-    list:any[]=[];
-     add(item:String)
-      {
-        
-  this.list.push({item});
-  console.log(this.list)
+  title = 'first_project';
+  list:any[]=[];
+  listd:any[]=[];
   
-    }
-    remove(i:number)
+  EnterSubmit(item:String,bo:any,date:any)
     {
-  this.list.splice(i,1);
-    }
+      
+      if(item!="")
+this.list.push({item});
+this.listd.push({date});
+bo.value=""
+
+}
+  remove(i:number)
+  {
+this.list.splice(i,1);
+this.listd.splice(i,1);
   }
+}
